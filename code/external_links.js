42580fa21a6bf5b49f3314f9ca38b48af0e39888
DavidKingNT@gmail.com <DavidKingNT@gmail.com@c71c1094-92aa-11de-9b7d-c11b5a55d12f>
2011-05-21 17:07:13 +0000

    deleted deprecated folder

--- code/external_links.js
+++
@@ -1,1 +0,0 @@
-function openExternal(){if(!document.getElementsByTagName){return }var A=document.getElementsByTagName("a");for(var B=0;B<A.length;B++){var C=A[B];if(C.getAttribute("href")&&C.getAttribute("rel")=="external"){C.target="_blank"}}}window.onload=openExternal;
